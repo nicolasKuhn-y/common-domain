dcb8547bb99bad9da1faee0f0494a65866a5f618
NEORIS\nicolas.kuhn <nicolas.kuhn@neoris.com>
2022-10-19 17:33:06 -0300

    modifico abstract event

--- src/events/event.ts
+++ src/events/event.ts
@@ -1,18 +1,36 @@
 import { Id } from '../value-objects'
 
 type EventInput = {
-  id: Id
-  type: string
-}
+  id: Id;
+  type: string;
+  attributes: { [key: string]: any };
+  meta?: { [key: string]: any };
+};
 
 export abstract class DomainEvent {
-  public readonly id: string
-  public readonly type: string
-  public readonly timestamp: Date
+  private readonly id: string
+  private readonly type: string
+  private readonly timestamp: Date
+  private readonly attributes: { [key: string]: any }
+  private readonly meta:{ [key: string]: any }
 
   constructor (props: EventInput) {
     this.id = props.id.value
     this.type = props.type
     this.timestamp = new Date()
+    this.attributes = props.attributes
+    this.meta = props.meta || {}
+  }
+
+  public get value () {
+    return {
+      data: {
+        id: this.id,
+        type: this.type,
+        timestamp: this.timestamp,
+        attributes: this.attributes
+      },
+      meta: this.meta
+    }
   }
 }
